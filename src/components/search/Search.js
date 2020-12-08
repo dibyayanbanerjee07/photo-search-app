1b829f89a0a368354ce08613a446a03fa94b5aea
Dibyayan Banerjee <banerjee.dib775@gmail.com>
2020-12-08 19:12:08 +0530

    first commit

---
+++ src/components/search/Search.js
@@ -0,0 +1,81 @@
+import React,{Component} from 'react';
+import axios from 'axios';
+import ImageResults from "../imageResults/imageResults";
+import Typography from '@material-ui/core/Typography';
+class Search extends Component{
+    state={
+        searchText:'',
+        apiUrl:'https://pixabay.com/api',
+        apiKey:'17241914-90da7b93c0ccceb734849dcd1',
+        images:[]
+    };
+    onTextChange=(e)=>{
+        const val=e.target.value;
+        this.setState({[e.target.name]:val},()=>{
+            if(val==='')
+            {
+                this.setState({images:[]});
+            }
+            else{
+            axios
+            .get(
+                `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+                    this.state.searchText
+                }&image_type=photo&safesearch=true`
+            )
+            .then(res=>this.setState({images:res.data.hits}))
+            .catch(err=>console.log(err));
+            }
+        });
+    };
+    render(){
+        console.log(this.state.images);
+        return(
+            <>
+             <div>
+                <input type="text" 
+                style=
+                {{backgroundColor:'black',
+                color:'white',
+                marginLeft:500,
+                marginTop:75,
+                paddingTop:20,
+                paddingLeft:70,
+                fontSize:30,
+                borderTopStyle:"hidden",
+                borderRightStyle:"hidden",
+                borderLeftStyle:"hidden",
+                outline:"none",
+                borderBottomStyle:"groove",
+
+            
+            }}
+            placeholder="Search any images"
+            name="searchText"
+            value={this.state.searchText}
+            onChange={this.onTextChange}
+            />
+            <br />
+            {this.state.images.length>0?(<ImageResults images={this.state.images}/>):null}
+          </div>
+          <div>
+            <Typography variant="h1" 
+             style={{
+                textAlign: 'center',
+                fontSize: 40,
+                color: 'white',
+                marginLeft: 100,
+                marginTop: 100
+                 }}
+                 >
+				Welcome To Photo Search App
+			</Typography>
+          </div>
+        </> 
+        )
+    }
+}
+
+
+
+export default Search;
